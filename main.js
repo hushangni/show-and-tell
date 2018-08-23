7659107e1f03a213e27fcd2ae80b10e4bb9f700c
Shangni <shangnihu@gmail.com>
2018-08-22 21:39:00 -0400

    Added basic styling and javascript comments for konami code

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-
-console.log('ready');
-
 // word konami
+// empty array to track the keys user will press
 const pressed = [];
+// secret code to match
 const secretCode = 'konami';
-window.addEventListener('keyup', (e) => {
-    console.log(e.key);
+
+window.addEventListener('keyup', function(e) {
+    // push every key pressed by user into pressed
     pressed.push(e.key);
-    pressed.splice(-secretCode.length - 1, pressed.length - secretCode);
-    console.log(pressed);
+    // cutting the array of pressed to only include the most recent
+    // pressed keys, up until the length of the secret code
+    // if pressed.length < secretCode.length, no cutting will be done
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
     if (pressed.join('').includes(secretCode)) {
         console.log('DING DING!');
-        cornify_add();
+        doSpecialThing();
     }
+
+    console.log(pressed);
 });
 
-// a key map of allowed keys
-var allowedKeys = {
+// object of keys involved in konami code
+const konamiKeys = {
     37: 'left',
     38: 'up',
     39: 'right',
@@ -25,40 +30,40 @@ var allowedKeys = {
     66: 'b'
 };
 
-// the 'official' Konami Code sequence
-var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+// the classic konami
+const konami = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
 
-// a variable to remember the 'position' the user has reached so far.
-var konamiCodePosition = 0;
+// position variable for markng which point of the sequence we are at
+let konamiIndex = 0;
 
-// add keydown event listener
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keyup', function(e) {
     // get the value of the key code from the key map
-    var key = allowedKeys[e.keyCode];
+    const key = konamiKeys[e.keyCode];
     // get the value of the required key from the konami code
-    var requiredKey = konamiCode[konamiCodePosition];
-
+    const keyNeeded = konami[konamiIndex];
     // compare the key with the required key
-    if (key == requiredKey) {
-
-        // move to the next key in the konami code sequence
-        konamiCodePosition++;
-
-        // if the last key is reached, activate cheats
-        if (konamiCodePosition == konamiCode.length) {
-            activateCheats();
-            konamiCodePosition = 0;
+    if (key == keyNeeded) {
+        konamiIndex++;
+        // if at the end of sequence, do your KONAMI ACTION
+        if (konamiIndex == konami.length) {
+            doSpecialThing();
+            konamiIndex = 0; // reset konami code index
         }
     } else {
-        konamiCodePosition = 0;
+        konamiIndex = 0; // reset konami code index
     }
 });
 
-function activateCheats() {
+const doSpecialThing = () => {
     document.body.style.backgroundImage = "url('images/cheatBackground.png')";
 
     var audio = new Audio('audio/pling.mp3');
     audio.play();
 
-    alert("cheats activated");
+    alert("doing special thing");
 }
+
+
+$(function () {
+    $('a').smoothScroll();
+});
